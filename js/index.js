d9be221ac85fd2cdace43f8fd7d3df6db914ba37
xuanyu liu <t3lixu00@students.oamk.fi>
2024-03-24 15:04:08 +0200

    Update index.js
    backend url

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-
-const BACKEND_ROOT_URL = 'http://localhost:3001'
+const BACKEND_ROOT_URL = 'https://todolist-backend-x2ae.onrender.com'
+// const BACKEND_ROOT_URL = 'http://localhost:3001'
 import { Todos } from "./class/Todos.js"
 
 const todos = new Todos(BACKEND_ROOT_URL)
