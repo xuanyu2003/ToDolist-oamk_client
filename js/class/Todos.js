ef9da4cbb4da8e734e3d2fa0e486f384250fe9ae
xuanyu2003 <t3lixu00@students.oamk.fi>
2024-03-24 13:45:37 +0200

    Initial commit

---
+++ js/class/Todos.js
@@ -0,0 +1,81 @@
+//Todos.js
+
+import { Task } from "./Task.js";
+
+class Todos {
+    #tasks = []
+    #backend_url = ''
+
+    constructor(url) {
+        this.#backend_url = url
+    }
+
+    getTasks = () => {
+        return new Promise(async(resolve, reject) => {
+            fetch(this.#backend_url)
+            .then((response) => response.json())
+            .then((json) => {
+                this.#readJson(json)
+                resolve(this.#tasks)
+            },(error) => {
+                reject(error)
+            })
+        })
+    }
+
+    addTask = (text) => {
+        return new Promise(async(resolve, reject) => {
+            const json = JSON.stringify({description: text})
+            fetch(this.#backend_url + '/new',{
+                method: 'post',
+                headers: {'Content-Type':'application/json'},
+                body: json
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                resolve(this.#addToArray(json.id,text))
+            },(error) => {
+                reject(error)
+            })
+        })
+    }
+
+    removeTask = (id) =>{
+        return new Promise(async(resolve, reject) =>{
+            fetch(this.#backend_url + '/delete/'+ id,{
+                method: 'delete',
+            })
+            .then((response) => response.json())
+            .then((json) => {
+              this.#removerFromArrary(id)
+              resolve(json.id)
+            },(error) => {
+                reject(error)
+            })
+        })
+    }
+
+    #readJson = (tasksAsJson) => {
+        tasksAsJson.forEach(node => {
+            const task = new Task(node.id,node.description)
+            this.#tasks.push(task)
+        })
+    }
+
+
+
+    
+
+    #addToArray = (id,text) => {
+        const task = new Task(id,text)
+        this.#tasks.push(task)
+        return task
+    }
+
+    #removerFromArrary =(id) =>{
+        const arrayWithoutRemoved = this.#tasks.filter(task=> task.id !== id)
+        this.#tasks =arrayWithoutRemoved
+    }
+}
+
+export { Todos }
---
+++ js/index.js
@@ -0,0 +1,87 @@
+
+const BACKEND_ROOT_URL = 'http://localhost:3001'
+import { Todos } from "./class/Todos.js"
+
+const todos = new Todos(BACKEND_ROOT_URL)
+
+const list = document.querySelector('ul')
+const input = document.querySelector('input')
+
+input.disabled = false
+
+const renderTask = (task) => {
+    const li = document.createElement('li')
+    li.setAttribute('class','list-group-item')
+    li.setAttribute('data-key',task.getId().toString())
+    // li.innerHTML =task.getText()
+    renderSpan(li,task.getText())
+    renderLink(li,task.getId())
+    list.append(li)
+}
+
+const renderSpan = (li,text) => {
+    const span = li.appendChild(document.createElement('span'))
+    span.innerHTML = text
+}
+
+const renderLink = (li,id) => {
+    const a = li.appendChild(document.createElement('a'))
+    a.innerHTML = '<i class="bi bi-trash"></i>'
+    a.setAttribute('style','float: right')
+    a.addEventListener('click',(event)=>{
+        todos.removeTask(id).then((removed_id)=>{
+            const li_to_remove = document.querySelector(`[data-key='${removed_id}']`)
+            if(li_to_remove){
+                list.removeChild(li_to_remove)
+            }
+        }).catch((error)=>{
+            alert(error)
+        })
+    })
+}
+
+
+const getTasks = () => {
+    todos.getTasks().then((tasks) => {
+        tasks.forEach(task => {
+            renderTask(task)
+        })
+    }).catch((error) => {
+        alert(error)
+    })
+}
+
+
+const saveTask =async (task) => {
+    try{
+        const json = JSON.stringify({descrption: task})
+        const response = await fetch(BACKEND_ROOT_URL + '/new',{
+            method: 'post',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: json
+        })
+        return response.json()
+    } catch(error){
+        alert("Error saving task: " + error.message)
+    }
+}
+
+input.addEventListener('keypress',(event) => {
+    if(event.key === 'Enter'){
+        event.preventDefault()
+        const task = input.value.trim()
+        if (task !== ''){
+           
+            todos.addTask(task).then((task) => {
+                renderTask(task)
+                input.value = ''
+                input.focus()
+            })
+           
+        }
+    }
+})
+
+getTasks()
